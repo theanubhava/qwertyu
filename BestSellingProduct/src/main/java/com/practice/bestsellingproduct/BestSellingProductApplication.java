68584da008594d53732acd037ef0dcabf60e0cc6
theanubhava <theanubhava@gmail.com>
2019-11-11 02:01:27 +0530

    Create BestSellingProductApplication.java

---
+++ BestSellingProduct/src/main/java/com/practice/bestsellingproduct/BestSellingProductApplication.java
@@ -0,0 +1,14 @@
+package com.practice.bestsellingproduct;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestSellingProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestSellingProductApplication.class, args);
+    }
+
+}
